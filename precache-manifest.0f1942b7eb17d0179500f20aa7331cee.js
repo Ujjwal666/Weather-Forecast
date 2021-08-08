24007cdf1148c6e667e828da72f3c74c4c24ef7e
Ujjwal666 <ujjwal.adhikari@bison.howard.edu>
2021-08-08 08:11:31 -0400

    Updates

---
+++ precache-manifest.0f1942b7eb17d0179500f20aa7331cee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4b1c0338fe058867379c4db5fa3b68",
+    "url": "/Weather-Forecast/index.html"
+  },
+  {
+    "revision": "1d7077dbb2eae00a2489",
+    "url": "/Weather-Forecast/static/css/main.3260e7e3.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/Weather-Forecast/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Forecast/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7077dbb2eae00a2489",
+    "url": "/Weather-Forecast/static/js/main.dd0f2cc7.chunk.js"
+  },
+  {
+    "revision": "da76584c6bfab72dfe8c",
+    "url": "/Weather-Forecast/static/js/runtime-main.41cd9640.js"
+  }
+]);
